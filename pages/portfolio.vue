6ef2ae52a51e8f60129ab45d1f36f45e9f0579e5
George <tang19970214@gmail.com>
2023-02-12 09:49:48 +0000

    none

--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -49,7 +49,7 @@ export default {
   methods: {
     getList() {
       let arr = [];
-      for (let i = 1; i <= 120; i++) {
+      for (let i = 1; i <= 150; i++) {
         arr.push(i);
       }
       // 亂數排序
